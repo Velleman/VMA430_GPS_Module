57121f0c4477101266a376fd6ecabba82fa4811b
Midas <midasgossye@hotmail.com>
2021-02-08 09:01:30 +0100

    Added UBX location and time decoding

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# VMA430 - GPS MODULE U-BLOX NEO-7M
+# Whadda WPI430 - GPS MODULE U-BLOX NEO-7M
 
-This repository contains the library and example code to work with our VMA430 - gPS MODULE U-BLOX NEO-7M
+This repository contains the library and example code to work with our Whadda WPI430 GPS MODULE with the U-BLOX NEO-7M chipset.
+
+## Installation
+
+1. Download the most recent version of the Arduino library by clicking the ```Download ZIP``` option in the Code menu:
+![](./images/download.jpg)
+2. Unzip the folder, and r
--- examples/VMA430_code/VMA430_code.ino
+++ examples/VMA430_code/VMA430_code.ino
@@ -8,10 +8,36 @@ void setup()
   Serial.begin(9600);
   Serial.println("hello");
   gps.begin(9600);
+  gps.setUBXNav();
+  gps.getUBX_packet();
 }
 
 void loop()
 {
+  //gps.getconfig();
+  if(gps.getUBX_packet())
+  {
+    gps.parse_ubx_data();
+    if(gps.utc_time.valid)
+    {
+      Serial.println();
+      Serial.print("UTC time: ");
+      Serial.print(gps.utc_time.hour);
+      Serial.print(":");
+      Serial.print(gps.utc_time.minute);
+      Serial.print(":");
+      Serial.print(gps.utc_time.second);
+      Serial.println();
+    }
+     Serial.println();
+    Serial.print("location: ");
+    Serial.print("Lat: ");
+    Serial.print(gps.location.latitude, 8);
+     Serial.print(" Long: ");
+     Serial.print(gps.location.longitude, 8);
+  }
+  
+   
   delay(10);
 }
 
